6c5d5fca41055fc69a4c15465a0a2526761790d1
Azhar-Iqbal2942 <azhariqbal2942@gmail.com>
2022-01-02 19:36:23 +0500

    Customer Details View added

--- app/store/urls/customer_urls.py
+++ app/store/urls/customer_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 # Custom Imports
-from store.views.customer_views import CustomerList
+from store.views.customer_views import CustomerList,CustomerDetail
 
 urlpatterns = [
-    path('',CustomerList.as_view())
+    path('',CustomerList.as_view()),
+    path('<int:pk>',CustomerDetail.as_view())
    
 ]
--- app/store/views/customer_views.py
+++ app/store/views/customer_views.py
@@ -1,16 +1,16 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
 
 # Custom Imports
 from store.models import Customer
 from store.serializers.customer_serializer import CustomerSerializer
 
-class CustomerList(APIView):
+class CustomerList(ListCreateAPIView):
     permission_classes = (AllowAny,)
-    
-    def get(self,request):
-        queryset = Customer.objects.all()
-        serializer = CustomerSerializer(queryset,many=True)
-        return Response(data=serializer.data)
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer 
+
+class CustomerDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer 
